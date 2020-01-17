8f84760d76d949c82953f9eadc2f9de9f26591ad
Madhavi Mallela <madhu2277@gmail.com>
2020-01-17 13:00:36 -0600

    Update initializeFileSystem.scala

--- initializeFileSystem.scala
+++ initializeFileSystem.scala
@@ -1,3 +1,9 @@
 spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
 dbutils.fs.ls("abfss://<file-system-name>@<storage-account-name>.dfs.core.windows.net/")
 spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
+
+# with ADB runtime version 5.1 or above you can also use OAuth 2 to authenticate the filesystem to initialize
+
+spark.conf.set(
+  "fs.azure.account.key.<storage-account-name>.dfs.core.windows.net",
+  dbutils.secrets.get(scope = "<scope-name>", key = "<key-name-for-service-credential>"))
